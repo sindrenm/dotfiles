ee1e10bd5ce11cfe465760e107e7fc19eb88de55
Sindre Moen <sindrenm@gmail.com>
2018-03-26 10:59:39 +0200

    Add Flutter to PATH

--- base/zsh/env.zsh
+++ base/zsh/env.zsh
@@ -4,6 +4,8 @@ export PATH="$HOME/.pyenv/bin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="/opt/android-sdk/platform-tools:$PATH"
 
+export PATH="/opt/flutter/bin:$PATH"
+
 export EDITOR=nvim
 
 export TERM=xterm-256color
