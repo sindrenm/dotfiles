cea28b9438a8829efff41af9106e72142087bd4a
Sindre Moen <sindrenm@gmail.com>
2021-04-06 21:01:45 +0200

    Add some paths to fish shell

--- base/fish/config.fish
+++ base/fish/config.fish
@@ -1,8 +1,7 @@
+source $HOME/.config/fish/path.fish
 source $HOME/.config/fish/aliases.fish
 source $HOME/.config/fish/env.fish
 source $HOME/.config/fish/init-scripts.fish
 
 # Init Starship prompt
 starship init fish | source
-
-neofetch
---
+++ base/fish/path.fish
@@ -0,0 +1,3 @@
+contains ~/.local/lib/dots/bin; or set -Ua fish_user_paths ~/.local/lib/dots/bin
+contains ~/.local/lib/nodenv/bin; or set -Ua fish_user_paths ~/.local/lib/nodenv/bin
+contains /opt/android-sdk/platform-tools; or set -Ua fish_user_paths /opt/android-sdk/platform-tools
