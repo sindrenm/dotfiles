df599db7d9a2cb01c2f380240d92f87b930e861a
Sindre Moen <sindrenm@gmail.com>
2024-12-21 21:41:56 +0100

    Configure wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'JetBrains Darcula'
+config.font = wezterm.font('JetBrains Mono')
+
+config.enable_tab_bar = false
+
+return config
