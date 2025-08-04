bd01d3bae3904e29f1069fdde5ff19c630d8ae08
Sindre Moen <sindrenm@gmail.com>
2025-08-05 00:28:20 +0200

    Add some comments in the Vim options config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,10 +1,26 @@
+-- Use 2 spaces for indentation
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+
+-- Show rounded borders around floating windows
 vim.opt.winborder = "rounded"
-vim.opt.ignorecase = true
+
+-- Show relative line numbers, but with the actual line number on the current line
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
+
+-- Always show the sign column to avoid jumping
 vim.opt.signcolumn = "yes"
+
+-- Make searches case-insensitive, unless at least one capitalized letter is used
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Enable GUI colors in the terminal
 vim.opt.termguicolors = true
+
+-- Preview all substitutions in a split
+vim.opt.inccommand = "split"
+
+-- Allow cursor on non-character lines
 vim.opt.virtualedit = "all"
