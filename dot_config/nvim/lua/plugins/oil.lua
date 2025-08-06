1df3f14c5d8bf416e34093a63b2eba672b031ca2
Sindre Moen <sindrenm@gmail.com>
2025-08-06 20:34:30 +0200

    Configure oiv.nvim

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,15 @@
 vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
 
-require("oil").setup()
+require("oil").setup({
+  default_file_explorer = true,
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+
+  view_options = {
+    natural_order = true,
+  },
+
+  win_options = {
+    wrap = true,
+  }
+})
