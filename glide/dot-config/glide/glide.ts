0533ed6db41745c0a188ce5eeeb40db198db5f47
Sindre Moen <sindrenm@gmail.com>
2026-05-04 08:50:24 +0200

    Glide: Add tab management keybindings

--- glide/dot-config/glide/glide.ts
+++ glide/dot-config/glide/glide.ts
@@ -21,3 +21,28 @@ glide.o.hint_size = "16px";
 // Remove bindings that mess with “enter search”
 glide.keymaps.del(["normal", "insert"], "<C-k>");
 glide.keymaps.del(["normal", "insert"], "<C-j>");
+
+// Tab management
+glide.keymaps.set("normal", "<leader>tn", () => {
+  glide.excmds.execute("tab_new");
+});
+
+glide.keymaps.set("normal", "<leader>ty", () => {
+  glide.excmds.execute("tab_duplicate");
+});
+
+glide.keymaps.set("normal", "<leader>tc", () => {
+  glide.excmds.execute("tab_close");
+});
+
+glide.keymaps.set("normal", "<leader>tp", () => {
+  glide.excmds.execute("tab_pin_toggle");
+});
+
+glide.keymaps.set("normal", "[t", () => {
+  glide.excmds.execute("tab_prev");
+});
+
+glide.keymaps.set("normal", "]t", () => {
+  glide.excmds.execute("tab_next");
+});
